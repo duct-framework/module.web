90eced878446bb9b985e19d2a4362a0f2a913318
James Reeves <jreeves@weavejester.com>
2017-02-01 21:58:30 +0000

    Add Jetty server dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [duct/server.http.jetty "0.1.0-SNAPSHOT"]
                  [compojure "1.5.1"]
                  [integrant "0.1.5"]
                  [ring/ring-core "1.5.0"]]
