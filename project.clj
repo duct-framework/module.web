fab10c0543c5b31e9766674a4b2fa710708a60d7
James Reeves <jreeves@weavejester.com>
2017-02-01 20:18:00 +0000

    Add middleware from Duct project

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.5.1"]
+                 [integrant "0.1.5"]
+                 [ring/ring-core "1.5.0"]]
+  :profiles
+  {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
