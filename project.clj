de968e9101af6ad04ae2195267d97c479f4d929d
James Reeves <jreeves@weavejester.com>
2017-02-03 14:51:54 +0000

    Add :duct.module.web/api key

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [duct/server.http.jetty "0.1.0-SNAPSHOT"]
                  [compojure "1.5.1"]
-                 [integrant "0.1.5"]
-                 [ring/ring-core "1.5.0"]]
+                 [integrant "0.2.0"]
+                 [meta-merge "1.0.0"]
+                 [ring/ring-core "1.5.1"]
+                 [ring/ring-defaults "0.2.2"]]
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
--- src/duct/module/web.clj
+++ src/duct/module/web.clj
@@ -1,6 +1,45 @@
-(ns duct.module.web)
+(ns duct.module.web
+  (:require [duct.core.web :as core]
+            [duct.middleware.web :as mw]
+            [duct.server.http.jetty :as jetty]
+            [integrant.core :as ig]
+            [meta-merge.core :refer [meta-merge]]
+            [ring.middleware.defaults :refer [api-defaults]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- not-in? [m ks]
+  (= (get-in m ks ::missing) ::missing))
+
+(defn- assoc-in-default [config keys default]
+  (cond-> config (not-in? config keys) (assoc-in keys default)))
+
+(defn- missing-middleware? [middleware key]
+  (not (contains? (set (map :key middleware)) key)))
+
+(defn- conj-middleware [middleware key]
+  (cond-> middleware (missing-middleware? middleware key) (conj (ig/ref key))))
+
+(defn- add-server [config {:keys [port] :or {port 3000}}]
+  (if-let [[[k v]] (ig/find-derived config :duct.server/http)]
+    (assoc-in-default config [k :port] port)
+    (assoc config :duct.server.http/jetty {:port port})))
+
+(defn- add-handler [config]
+  (let [[[k v]] (ig/find-derived config :duct.server/http)]
+    (-> config
+        (assoc-in [k :handler] (ig/ref ::core/handler))
+        (assoc-in-default [::core/handler :endpoints]  [])
+        (assoc-in-default [::core/handler :middleware] []))))
+
+(defn add-middleware [config key value]
+  (-> config
+      (update-in [::core/handler :middleware] conj-middleware key)
+      (update key (partial meta-merge value))))
+
+(defmethod ig/init-key ::api [_ options]
+  (fn [config]
+    (-> config
+        (add-server options)
+        (add-handler)
+        (add-middleware ::mw/not-found   {:response "Resource Not Found"})
+        (add-middleware ::mw/defaults    api-defaults)
+        (add-middleware ::mw/hide-errors {:response "Internal Server Error"}))))
