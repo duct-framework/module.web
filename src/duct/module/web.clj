717ee363b77904919f3055290bbfd156eb91c5d0
James Reeves <jreeves@weavejester.com>
2017-02-08 19:34:43 +0000

    Make add-middleware function more flexible

--- src/duct/module/web.clj
+++ src/duct/module/web.clj
@@ -11,8 +11,8 @@
 (defn- missing-middleware? [middleware key]
   (not (contains? (set (map :key middleware)) key)))
 
-(defn- conj-middleware [middleware key]
-  (cond-> middleware (missing-middleware? middleware key) (conj (ig/ref key))))
+(defn- update-middleware [middleware func key]
+  (cond-> middleware (missing-middleware? middleware key) (func (ig/ref key))))
 
 (defn- add-server [config {:keys [server-port] :or {server-port 3000}}]
   (if-let [[k v] (ig/find-derived-1 config :duct.server/http)]
@@ -26,9 +26,9 @@
         (assoc-in-default [::core/handler :endpoints]  [])
         (assoc-in-default [::core/handler :middleware] []))))
 
-(defn add-middleware [config key value]
+(defn add-middleware [config func key value]
   (-> config
-      (update-in [::core/handler :middleware] conj-middleware key)
+      (update-in [::core/handler :middleware] update-middleware func key)
       (update key (partial meta-merge value))))
 
 (defn- get-env [config options]
@@ -37,16 +37,16 @@
 (defn- add-error-middleware [config options response]
   (let [env (get-env config options)]
     (cond-> config
-      (= env :production)  (add-middleware ::mw/hide-errors {:response response})
-      (= env :development) (add-middleware ::mw/stacktrace  {}))))
+      (= env :production)  (add-middleware conj ::mw/hide-errors {:response response})
+      (= env :development) (add-middleware conj ::mw/stacktrace  {}))))
 
 (defmethod ig/init-key ::api [_ options]
   (fn [config]
     (-> config
         (add-server options)
         (add-handler)
-        (add-middleware ::mw/not-found {:response "Resource Not Found"})
-        (add-middleware ::mw/defaults  defaults/api-defaults)
+        (add-middleware conj ::mw/not-found {:response "Resource Not Found"})
+        (add-middleware conj ::mw/defaults  defaults/api-defaults)
         (add-error-middleware options  "Internal Server Error"))))
 
 (def ^:private error-404 (io/resource "duct/module/web/errors/404.html"))
@@ -61,7 +61,7 @@
     (-> config
         (add-server options)
         (add-handler)
-        (add-middleware ::mw/not-found {:response error-404})
-        (add-middleware ::mw/webjars   {})
-        (add-middleware ::mw/defaults  (site-defaults options))
+        (add-middleware conj ::mw/not-found {:response error-404})
+        (add-middleware conj ::mw/webjars   {})
+        (add-middleware conj ::mw/defaults  (site-defaults options))
         (add-error-middleware options error-500))))
