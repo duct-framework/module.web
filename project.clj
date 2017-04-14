4c3618ef197df70607abe3a346e5e85a8916243c
James Reeves <jreeves@weavejester.com>
2017-04-14 17:33:05 +0100

    Update Ring dependencies

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/core "0.9.0-SNAPSHOT"]
                  [duct/server.http.jetty "0.1.0-SNAPSHOT"]
-                 [compojure "1.5.1"]
+                 [compojure "1.6.0-beta3"]
                  [integrant "0.2.1"]
                  [meta-merge "1.0.0"]
                  [org.slf4j/slf4j-nop "1.7.21"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.5.1"]
-                 [ring/ring-devel "1.5.1"]
-                 [ring/ring-defaults "0.2.3"]
+                 [ring/ring-core "1.6.0-RC2"]
+                 [ring/ring-devel "1.6.0-RC2"]
+                 [ring/ring-defaults "0.3.0-beta3"]
                  [ring-webjars "0.1.1"]]
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
