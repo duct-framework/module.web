3252515908c30e48ca090495a9cc1a7d3807cbd1
James Reeves <jreeves@weavejester.com>
2017-04-14 22:28:07 +0100

    Refactor module with merge-configs

--- src/duct/module/web.clj
+++ src/duct/module/web.clj
@@ -1,42 +1,19 @@
 (ns duct.module.web
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
-            [duct.core :refer [assoc-in-default]]
+            [duct.core :as core]
             [duct.core.env :as env]
-            [duct.core.web :as core]
+            [duct.core.merge :as merge]
+            [duct.core.web :as web]
             [duct.middleware.web :as mw]
             [duct.server.http.jetty :as jetty]
             [integrant.core :as ig]
-            [meta-merge.core :refer [meta-merge]]
             [ring.middleware.defaults :as defaults]))
 
 (def ^:private server-port
   (env/env '["PORT" Int :or 3000]))
 
-(defn- missing-middleware? [middleware key]
-  (not (contains? (set (map :key middleware)) key)))
-
-(defn- update-middleware [middleware func key]
-  (cond-> middleware (missing-middleware? middleware key) (func (ig/ref key))))
-
-(defn- add-server [config]
-  (if-let [[k v] (ig/find-derived-1 config :duct.server/http)]
-    (assoc-in-default config [k :port] server-port)
-    (assoc config :duct.server.http/jetty {:port server-port})))
-
-(defn- add-handler [config]
-  (let [[k v] (ig/find-derived-1 config :duct.server/http)]
-    (-> config
-        (assoc-in [k :handler] (ig/ref ::core/handler))
-        (assoc-in-default [::core/handler :endpoints]  [])
-        (assoc-in-default [::core/handler :middleware] []))))
-
-(defn add-middleware [config func key value]
-  (-> config
-      (update-in [::core/handler :middleware] update-middleware func key)
-      (update key (partial meta-merge value))))
-
-(defn- get-env [config options]
+(defn- get-environment [config options]
   (:environment options (:duct.core/environment config :production)))
 
 (defn- get-project-ns [config options]
@@ -45,47 +22,68 @@
 (defn- name-to-path [sym]
   (-> sym name (str/replace "-" "_") (str/replace "." "/")))
 
-(defn- add-log-middleware [config]
-  (let [[server _] (ig/find-derived-1 config :duct.server/http)]
-    (if-let [[logger _] (ig/find-derived-1 config :duct/logger)]
-      (-> config
-          (assoc-in-default [server :logger] (ig/ref logger))
-          (add-middleware conj ::mw/log-requests {:logger (ig/ref logger)})
-          (add-middleware conj ::mw/log-errors   {:logger (ig/ref logger)}))
-      config)))
+(defn- derived-key [m k default]
+  (if-let [kv (ig/find-derived-1 m k)] (key kv) default))
 
-(defn- add-error-middleware [config options response]
-  (let [env (get-env config options)]
-    (cond-> config
-      (= env :production)  (add-middleware conj ::mw/hide-errors {:response response})
-      (= env :development) (add-middleware conj ::mw/stacktrace  {}))))
+(defn- http-server-key [config]
+  (derived-key config :duct.server/http :duct.server.http/jetty))
 
-(defmethod ig/init-key ::api [_ options]
-  (fn [config]
-    (-> config
-        (add-server options)
-        (add-handler)
-        (add-middleware conj ::mw/not-found {:response "Resource Not Found"})
-        (add-middleware conj ::mw/defaults  defaults/api-defaults)
-        (add-log-middleware)
-        (add-error-middleware options "Internal Server Error"))))
+(defn- server-config [config]
+  {(http-server-key config) {:port (merge/displace server-port)}})
+
+(def ^:private logging-config
+  {::web/handler     {:middleware ^:distinct [(ig/ref ::mw/log-requests)
+                                              (ig/ref ::mw/log-errors)]}
+   ::mw/log-requests {:logger (ig/ref ::core/logger)}
+   ::mw/log-errors   {:logger (ig/ref ::core/logger)}})
+
+(def ^:private error-configs
+  {:production
+   {::web/handler {:middleware ^:distinct [(ig/ref ::mw/hide-errors)]}}
+   :development
+   {::web/handler {:middleware ^:distinct [(ig/ref ::mw/stacktrace)]}}})
+
+(def ^:private api-config
+  {:duct.server/http {:handler (ig/ref ::web/handler)}
+   ::web/handler     {:endpoints  []
+                      :middleware ^:distinct [(ig/ref ::mw/not-found)
+                                              (ig/ref ::mw/defaults)]}
+   ::mw/not-found    {:response (merge/displace "Resource Not Found")}
+   ::mw/hide-errors  {:response (merge/displace "Internal Server Error")}
+   ::mw/defaults     (merge/displace defaults/api-defaults)})
 
 (def ^:private error-404 (io/resource "duct/module/web/errors/404.html"))
 (def ^:private error-500 (io/resource "duct/module/web/errors/500.html"))
 
-(defn- site-defaults [config options]
-  (let [dirs      (name-to-path (get-project-ns config options))
-        resources ["duct/module/web/public" (str dirs "/public")]]
-    (-> defaults/site-defaults
-        (assoc-in [:static :resources] resources))))
+(defn- site-resource-paths [project-ns]
+  ["duct/module/web/public" (str (name-to-path project-ns) "/public")])
+
+(defn- site-defaults [project-ns]
+  (assoc-in defaults/site-defaults [:static :resources] (site-resource-paths project-ns)))
+
+(defn- site-config [project-ns]
+  {:duct.server/http {:handler (ig/ref ::web/handler)}
+   ::web/handler     {:endpoints  []
+                      :middleware ^:distinct [(ig/ref ::mw/not-found)
+                                              (ig/ref ::mw/webjars)
+                                              (ig/ref ::mw/defaults)]}
+   ::mw/webjars      {}
+   ::mw/not-found    {:response (merge/displace error-404)}
+   ::mw/hide-errors  {:response (merge/displace error-500)}
+   ::mw/defaults     (merge/displace (site-defaults project-ns))})
+
+(defmethod ig/init-key ::api [_ options]
+  (fn [config]
+    (core/merge-configs config
+                        (server-config config)
+                        api-config
+                        logging-config
+                        (error-configs (get-environment config options)))))
 
 (defmethod ig/init-key ::site [_ options]
   (fn [config]
-    (-> config
-        (add-server)
-        (add-handler)
-        (add-middleware conj ::mw/not-found {:response error-404})
-        (add-middleware conj ::mw/webjars   {})
-        (add-middleware conj ::mw/defaults  (site-defaults config options))
-        (add-log-middleware)
-        (add-error-middleware options error-500))))
+    (core/merge-configs config
+                        (server-config config)
+                        (site-config (get-project-ns config options))
+                        logging-config
+                        (error-configs (get-environment config options)))))
--- test/duct/module/web_test.clj
+++ test/duct/module/web_test.clj
@@ -1,7 +1,86 @@
 (ns duct.module.web-test
-  (:require [clojure.test :refer :all]
-            [duct.module.web :refer :all]))
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [duct.core :as core]
+            [duct.module.web :as web]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def api-config
+  {:duct.core/project-ns  'foo
+   :duct.core/environment :production
+   :duct.core/modules     [(ig/ref :duct.module.web/api)]
+   :duct.module.web/api   {}})
+
+(def site-config
+  {:duct.core/project-ns  'foo
+   :duct.core/environment :production
+   :duct.core/modules     [(ig/ref :duct.module.web/site)]
+   :duct.module.web/site  {}})
+
+(deftest api-module-test
+  (is (= (core/prep api-config)
+         (merge api-config
+                {:duct.middleware.web/log-requests
+                 {:logger (ig/ref :duct.core/logger)}
+                 :duct.core.web/handler
+                 {:endpoints []
+                  :middleware
+                  [(ig/ref :duct.middleware.web/not-found)
+                   (ig/ref :duct.middleware.web/defaults)
+                   (ig/ref :duct.middleware.web/log-requests)
+                   (ig/ref :duct.middleware.web/log-errors)
+                   (ig/ref :duct.middleware.web/hide-errors)]}
+                 :duct.middleware.web/defaults
+                 {:params    {:urlencoded true, :keywordize true}
+                  :responses {:not-modified-responses true
+                              :absolute-redirects true
+                              :content-types true
+                              :default-charset "utf-8"}}
+                 :duct.server.http/jetty
+                 {:port 3000, :handler (ig/ref :duct.core.web/handler)}
+                 :duct.middleware.web/hide-errors
+                 {:response "Internal Server Error"}
+                 :duct.middleware.web/not-found
+                 {:response "Resource Not Found"}
+                 :duct.middleware.web/log-errors
+                 {:logger (ig/ref :duct.core/logger)}}))))
+
+(deftest site-module-test
+  (is (= (core/prep site-config)
+         (merge site-config
+                {:duct.middleware.web/log-requests
+                 {:logger (ig/ref :duct.core/logger)}
+                 :duct.core.web/handler
+                 {:endpoints []
+                  :middleware
+                  [(ig/ref :duct.middleware.web/not-found)
+                   (ig/ref :duct.middleware.web/webjars)
+                   (ig/ref :duct.middleware.web/defaults)
+                   (ig/ref :duct.middleware.web/log-requests)
+                   (ig/ref :duct.middleware.web/log-errors)
+                   (ig/ref :duct.middleware.web/hide-errors)]}
+                 :duct.middleware.web/defaults
+                 {:params   {:urlencoded true
+                             :multipart  true
+                             :nested     true
+                             :keywordize true}
+                  :cookies   true
+                  :session   {:flash true, :cookie-attrs {:http-only true}}
+                  :security  {:anti-forgery         true
+                              :xss-protection       {:enable? true, :mode :block}
+                              :frame-options        :sameorigin
+                              :content-type-options :nosniff}
+                  :static    {:resources ["duct/module/web/public" "foo/public"]}
+                  :responses {:not-modified-responses true
+                              :absolute-redirects     true
+                              :content-types          true
+                              :default-charset        "utf-8"}}
+                 :duct.server.http/jetty
+                 {:port 3000, :handler (ig/ref :duct.core.web/handler)}
+                 :duct.middleware.web/webjars {}
+                 :duct.middleware.web/hide-errors
+                 {:response (io/resource "duct/module/web/errors/500.html")}
+                 :duct.middleware.web/not-found
+                 {:response (io/resource "duct/module/web/errors/404.html")}
+                 :duct.middleware.web/log-errors
+                 {:logger (ig/ref :duct.core/logger)}}))))
