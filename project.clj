1bd3502463b92c0c9732eedd776eb3f63e193d0e
James Reeves <jreeves@weavejester.com>
2017-04-18 17:43:04 +0100

    Update Ring dependencies to 1.6.0-RC3

--- project.clj
+++ project.clj
@@ -6,14 +6,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [duct/core "0.1.1"]
                  [duct/logger "0.1.1"]
-                 [duct/server.http.jetty "0.1.1"]
+                 [duct/server.http.jetty "0.1.2"]
                  [compojure "1.6.0-beta3"]
                  [integrant "0.3.3"]
                  [org.slf4j/slf4j-nop "1.7.21"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.6.0-RC2"]
-                 [ring/ring-devel "1.6.0-RC2"]
+                 [ring/ring-core "1.6.0-RC3"]
+                 [ring/ring-devel "1.6.0-RC3"]
                  [ring/ring-defaults "0.3.0-beta3"]
-                 [ring-webjars "0.1.1"]]
+                 [ring-webjars "0.2.0-beta1"]]
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}})
