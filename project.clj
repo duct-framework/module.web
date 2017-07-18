2fe991f862c31e697ae849872dd32ada630593ee
James Reeves <jreeves@weavejester.com>
2017-07-18 10:56:27 +0100

    Update slf4j-nop version to 1.7.25

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [compojure "1.6.0"]
                  [integrant "0.4.1"]
                  [metosin/muuntaja "0.2.1"]
-                 [org.slf4j/slf4j-nop "1.7.21"]
+                 [org.slf4j/slf4j-nop "1.7.25"]
                  [org.webjars/normalize.css "5.0.0"]
                  [ring/ring-core "1.6.2"]
                  [ring/ring-devel "1.6.2"]
