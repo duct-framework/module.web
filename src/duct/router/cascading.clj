7c6629b1a5fd1e33ada8b8bd6bb98ac278d5ee1a
James Reeves <jreeves@weavejester.com>
2017-09-04 14:18:28 +0100

    Move router derive into duct_hierarchy.edn

--- src/duct/router/cascading.clj
+++ src/duct/router/cascading.clj
@@ -2,7 +2,5 @@
   (:require [compojure.core :as compojure]
             [integrant.core :as ig]))
 
-(derive :duct.router/cascading :duct/router)
-
 (defmethod ig/init-key :duct.router/cascading [_ routes]
   (apply compojure/routes routes))
