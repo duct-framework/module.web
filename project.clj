3eec8de8a17db69481697622fdf6ee65817743de
James Reeves <jreeves@weavejester.com>
2017-11-02 19:39:04 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/duct-framework/module.web"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
                  [duct/core "0.6.1"]
                  [duct/logger "0.2.1"]
                  [duct/server.http.jetty "0.2.0"]
@@ -12,8 +12,8 @@
                  [metosin/muuntaja "0.3.2"]
                  [org.slf4j/slf4j-nop "1.7.25"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.6.2"]
-                 [ring/ring-devel "1.6.2"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-devel "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [ring-webjars "0.2.0"]]
   :profiles
