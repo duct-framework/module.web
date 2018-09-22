37c198a48e2884f3dae91a28cfd79caa672f3ff3
James Reeves <jreeves@weavejester.com>
2018-09-22 13:59:36 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [duct/core "0.7.0-alpha5"]
+                 [duct/core "0.7.0-alpha6"]
                  [duct/logger "0.2.1"]
                  [duct/server.http.jetty "0.2.0"]
                  [compojure "1.6.1"]
@@ -12,8 +12,8 @@
                  [metosin/muuntaja "0.6.0"]
                  [org.slf4j/slf4j-nop "1.7.25"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-devel "1.6.3"]
+                 [ring/ring-core "1.7.0"]
+                 [ring/ring-devel "1.7.0"]
                  [ring/ring-defaults "0.3.2"]
                  [ring-webjars "0.2.0"]]
   :profiles
