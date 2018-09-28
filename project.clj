14a6bdfe36eb954ecdc0f6dc8c031cebc0a85e93
James Reeves <jreeves@weavejester.com>
2018-09-28 18:42:50 +0100

    Update Muuntaja to 0.6.1

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [duct/server.http.jetty "0.2.0"]
                  [compojure "1.6.1"]
                  [integrant "0.7.0"]
-                 [metosin/muuntaja "0.6.0"]
+                 [metosin/muuntaja "0.6.1"]
                  [org.slf4j/slf4j-nop "1.7.25"]
                  [org.webjars/normalize.css "5.0.0"]
                  [ring/ring-core "1.7.0"]
