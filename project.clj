c5f2b6cf96c76e54812b06f639331982ac01fcdb
Damian Hryniewicz <damian@magnet.coop>
2020-08-31 14:45:06 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "https://github.com/duct-framework/module.web"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [duct/core "0.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [duct/core "0.8.0"]
                  [duct/logger "0.3.0"]
-                 [duct/server.http.jetty "0.2.0"]
-                 [compojure "1.6.1"]
-                 [integrant "0.7.0"]
-                 [metosin/muuntaja "0.6.3"]
-                 [org.slf4j/slf4j-nop "1.7.25"]
+                 [duct/server.http.jetty "0.2.1"]
+                 [compojure "1.6.2"]
+                 [integrant "0.8.0"]
+                 [metosin/muuntaja "0.6.7"]
+                 [org.slf4j/slf4j-nop "1.7.30"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.7.1"]
-                 [ring/ring-devel "1.7.1"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-devel "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring-webjars "0.2.0"]]
   :profiles
-  {:dev {:dependencies [[ring/ring-mock "0.3.2"]]}})
+  {:dev {:dependencies [[ring/ring-mock "0.4.0"]]}})
