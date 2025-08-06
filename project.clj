9065f23e8e974d56abdfbef83aa10fb7f958075c
James Reeves <jreeves@weavejester.com>
2025-08-06 17:58:11 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -10,13 +10,13 @@
                  [org.duct-framework/router.reitit "0.5.1"]
                  [com.rpl/specter "1.1.4"]
                  [integrant "0.13.1"]
-                 [hiccup "2.0.0-RC5"]
+                 [hiccup "2.0.0"]
                  [org.slf4j/slf4j-nop "2.0.17"]
                  [org.webjars/normalize.css "5.0.0"]
-                 [ring/ring-core "1.14.1"]
-                 [ring/ring-devel "1.14.1"]
+                 [ring/ring-core "1.14.2"]
+                 [ring/ring-devel "1.14.2"]
                  [ring/ring-defaults "0.6.0"]
-                 [ring-webjars "0.3.0"]]
+                 [ring-webjars "0.3.1"]]
   :plugins [[eftest "0.6.0"]]
   :profiles
-  {:dev {:dependencies [[ring/ring-mock "0.6.1"]]}})
+  {:dev {:dependencies [[ring/ring-mock "0.6.2"]]}})
